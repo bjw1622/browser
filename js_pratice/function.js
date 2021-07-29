df6a6fc8c0402c37a0d8b45182bcc6dd65d8ad4f
Jay <63109802+bjw1622@users.noreply.github.com>
2021-07-29 14:58:43 +0900

    Learning function
    Learning function

---
+++ js_pratice/function.js
@@ -0,0 +1,47 @@
+//Function
+// -fundamental building block in the program
+
+
+// 1.Function CSSStyleDeclaration
+// function name(pra,1.param2){
+//   body --- return;
+// }
+//function is object in JS
+
+// 2.Parameters
+function changeName(obj){
+  obj.name = 'coder';
+}
+const jay = {name:'jay'};
+changeName('baek');
+console.log(jay)
+
+//3.Default parameters(added in ES6)
+//파라미터가 입력되지 않아도 default값 대체 지정
+function showMessage(message, from="jay"){
+  console.log(`${message} by ${from}`)
+}
+showMessage(`Hi`);
+
+//4.Rest parameters(added in ES6)
+function printall(...args){
+  for(let i =0; i < args.length; i++){
+    console.log(args[i])
+  }
+}
+printall('dream','coding','jay')
+
+//5.Local scope
+
+//6.return
+
+//7.Early return,early exist 
+
+//Arrow function
+const simple = () => {
+
+}
+//IIFE: Immediately Invoked Function Expression
+(function hello(){
+  console.log('IIFE')
+})();

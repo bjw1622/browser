12f2e11fac7617e2d74c47460ac6fd14283bec9b
Jay <63109802+bjw1622@users.noreply.github.com>
2021-08-07 23:05:50 +0900

    Complete shopping file
    Complete shopping file

--- 04.DOM/Shopping List/main.js
+++ 04.DOM/Shopping List/main.js
@@ -5,12 +5,18 @@ const addBtn = document.querySelector('.footer__button');
 function onAdd(){
       // 1. 사용자가 입력한 텍스트 받아오기
       const text = input.value;
-      console.log(text);
+      //아무것도 입력되지 않았을때
+      if(text === ''){
+        input.focus();
+        return;//추가하지않고 그냥 함수 나가버리기
+      }
       // 2. 새로운 아이템을 만듬 
       const item = createItem(text);
       // 3. 아이템 컨테이너 안에 추가
       items.appendChild(item);
-      // 4. 인풋 초기화
+      // 4.새로 추가된 아이템으로 스크롤링
+      item.scrollIntoView({block:'center'});
+      // 5. 인풋 초기화
       input.value = '';
       input.focus();
 }
@@ -47,4 +53,10 @@ function createItem(text){
 
 addBtn.addEventListener('click',()=>{
   onAdd();
+})
+
+input.addEventListener('keypress',(event)=>{
+  if(event.key === 'Enter'){
+    onAdd();
+  }
 })

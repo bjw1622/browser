235ba248a136d262edc8759e04c6d25167230b89
Jay <63109802+bjw1622@users.noreply.github.com>
2021-08-26 16:59:48 +0900

    Update Files
    Update Files

---
+++ js_pratice/json.js
@@ -0,0 +1,13 @@
+// JSON
+// JavaScript Object Notation
+
+//1.Object to JSON
+//stringfy(obj) object를 json으로 변환 
+let json = JSON.stringify(true);
+console.log(json);
+
+json = JSON.stringify([`apple`,`banana`]);
+console.log(json);
+
+
+//JSON to Objet
--- js_pratice/variable.js
+++ js_pratice/variable.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//2.Variable
+//2.Variable(read/write)
 // let(added in ES6)
 let globalName = 'global name';
 {
@@ -15,15 +15,18 @@ console.log(globalName)
 //var hoisting(선언이 제일 위로 올라간다.)
 //has no block scope
 
-// 3.Constants
+// 3.Constant(read only)
+//use const whenever possible
 //선언함과 동시에 할당한 후 값 변경 x
-//security
-//thread safety
-//reduce human mistakes
+//favor immutable data type always for a few reasons:
+//  -security
+//  -thread safety
+//  -reduce human mistakes
 const datsInWeek = 7;
 const maxNumber = 5;
 
 //4.Variable types
+//메모리 저장 타입 =>  primitive(value로 값 저장),object(object를 가르키는 reference가 저장)
 //primitive(더 이상 작은 단위로 나눠질 수 없는 것 ), single item:number,string,boolean,null,undefined,symbol
 //object , box container
 //function, first-calss function

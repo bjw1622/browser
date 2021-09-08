2887b4586001d4dadf21977a335b768e2189288b
Jay <63109802+bjw1622@users.noreply.github.com>
2021-09-08 16:21:43 +0900

    Add files via upload

--- js_pratice/async/callback.js
+++ js_pratice/async/callback.js
@@ -1,54 +1,55 @@
-'usr strict';
+"usr strict";
 
-//JavaScript is synchromous.
-//hoisiting: var 변수, function delcaration(선언)이 자동적으로 제일 위로 선언이 되는 것
-//hoising이 일어난 이후 코드 실행
+//JavaScript is synchromous. 동기적
+//hoisiting: var, function delcaration(선언)이 자동적으로 제일 위로 선언이 되는 것
+//hoising이 일어난 이후 하나하나 동기적으로 코드가 실행
 console.log(`1`);
-setTimeout(function(){
-  console.log('2');
-},1000)//browser api 
+setTimeout(function () {
+  console.log("2");
+}, 1000); //browser api, 브라우저에게 1초뒤에 함수를 실행시켜달라고 요청
+
 //setTimeout에 우리가 만든 함수를 파라미터로 전달
 console.log(`3`);
-
+// 1, 3, 2
 
 //Synchronous Call back function
 //함수는 호이스팅 되기 때문에 맨 위로
-function print(print){
+function print(print) {
   print();
 }
-print(() => {console.log(`hello`)});
+print(() => {
+  console.log(`hello`);
+});
 
 //Asynchronous Call back function
-function printDelay(print,time){
-  setTimeout(print,time);
+function printDelay(print, time) {
+  setTimeout(print, time);
 }
-printDelay(()=>console.log(`hi`),2000);
+printDelay(() => console.log(`hi`), 2000);
 
 //Callback Hell example
 
-class UserStorage{
-  loginUser(id,password,onSuccess,onError){
-    setTimeout(()=>{
-      if(
+class UserStorage {
+  loginUser(id, password, onSuccess, onError) {
+    setTimeout(() => {
+      if (
         (id === `ellie` && password === `dream`) ||
         (id === `coder` && password === `academy`)
-      ){
+      ) {
         onSuccess(id);
-      }
-      else{
+      } else {
         onError(new Error(`not found`));
       }
-    },2000)
+    }, 2000);
   }
-  getRoles(user,onSuccess,onError){
-    setTimeout(()=>{
-      if(user === `ellie`){
-        onSuccess({name:`ellie`,role:'admin'});
+  getRoles(user, onSuccess, onError) {
+    setTimeout(() => {
+      if (user === `ellie`) {
+        onSuccess({ name: `ellie`, role: "admin" });
+      } else {
+        onError(new Error(`no acess`));
       }
-      else{
-        onError(new Error(`no acess`))
-      }
-    },1000)
+    }, 1000);
   }
 }
 
@@ -56,4 +57,9 @@ const UserStorage = new UserStorage();
 const id = prompt(`enter your id`);
 const password = prompt(`enter your pw`);
 
-UserStorage.loginUser(id,password,(user)=>{},(error)=>{});
+UserStorage.loginUser(
+  id,
+  password,
+  (user) => {},
+  (error) => {}
+);
